497c5a86bc5af07cf1ab9a521986223a948f6b57
Amy Kapernick <amy@kapers.dev>
2021-09-28 15:29:36 +0800

    have react functionality working

--- site/src/js/components/app.js
+++ site/src/js/components/app.js
@@ -6,17 +6,22 @@ const useState = React.useState;
 const useRef = React.useRef
 const createRef = React.createRef
 
-const RecipePicker = () => {
+const RecipePicker = ({day, section}) => {
 	const [allRecipes, setAllRecipes] = useState([]);
 	const [modalOpen, toggleModal] = useState(false);
 	const [recipes, setRecipes] = useState([])
 
 	useEffect(() => {
 		if(typeof window !== 'undefined') {
-			const data = window.localStorage.getItem('recipes')
+			const recipeList = window.localStorage.getItem('recipes')
+			const savedRecipes = window.localStorage.getItem('savedRecipes')
 
-			if(data) {
-				setAllRecipes(JSON.parse(data))
+			if(recipeList) {
+				setAllRecipes(JSON.parse(recipeList))
+			}
+
+			if(savedRecipes) {
+				setRecipes(JSON.parse(savedRecipes)[day]?.[section] || [])
 			}
 		}
 	}, [])
@@ -25,16 +30,18 @@ const RecipePicker = () => {
 		<React.Fragment>
 			<button onClick={() => toggleModal(true)}>Select Recipes</button>
 			{recipes.map(recipe => (
-				<h3>{recipe.title}</h3>
+				<RecipeCard key={recipe.permalink} {...recipe} />
 			))}
-			{modalOpen && <Modal {...{recipes: allRecipes, toggleModal, setRecipes}} />}
+			{modalOpen && <Modal {...{recipes, allRecipes, toggleModal, setRecipes, day, section}} />}
 		</React.Fragment>
 	)
 }
 
-const Modal = ({recipes, toggleModal, setRecipes}) => {
+const Modal = ({allRecipes, toggleModal, setRecipes, recipes, day, section}) => {
 	const select = createRef()
+	const selectedRecipes = recipes.map(recipe => recipe.permalink)
 	const saveRecipes = () => {
+		const data = window.localStorage.getItem('savedRecipes') ? JSON.parse(window.localStorage.getItem('savedRecipes')) : {}
 		const recipeList = []
 		const options = select.current.selectedOptions
 
@@ -42,10 +49,21 @@ const Modal = ({recipes, toggleModal, setRecipes}) => {
 			recipeList.push(JSON.parse(opt.value))
 		}
 
+		const newData = {
+			...data,
+			[day]: {
+				...data?.[day],
+				[section]: recipeList
+			}
+		}
+
+		window.localStorage.setItem('savedRecipes', JSON.stringify(newData))
+
 		setRecipes(recipeList)
 
 		toggleModal(false)
 	}
+
 	return (
 		<div className="modal">
 			<button onClick={saveRecipes}>Save Recipes</button>
@@ -55,15 +73,45 @@ const Modal = ({recipes, toggleModal, setRecipes}) => {
 				name="recipes"
 				size="5"
 			>
-				{recipes.map((recipe) => (
-					<option key={recipe.permalink} value={JSON.stringify(recipe)}>{recipe.title}</option>
+				{allRecipes.map((recipe) => (
+					<option 
+						key={recipe.permalink} 
+						value={JSON.stringify(recipe)}
+						selected={selectedRecipes.includes(recipe.permalink)}
+					>
+						{recipe.title}
+					</option>
 				))}
 			</select>
 		</div>
 	)
 }
 
-document.querySelectorAll('.days .picker').forEach(section => {
-	ReactDOM.render(e(RecipePicker), section);
+const RecipeCard = ({title, image, categories, permalink}) => (
+	<article className="recipe_card">
+		<h2>
+			<a href={`/${permalink}`}>
+				{title}
+			</a>
+		</h2>
+		<img src={image || '/img/placeholder.png'} />
+		<ul className="tags" lang="en-AU">
+			{categories.map(cat => (
+				<li key={cat}>{cat}</li>
+			))}
+		</ul>
+	</article>
+)
+
+document.querySelectorAll('.days .day').forEach(day => {
+	const dayName = day.dataset.day
+
+	day.querySelectorAll('.sections .section').forEach(section => {
+		const sectionName = section.dataset.section
+
+		const picker = section.querySelector('.picker')
+
+		ReactDOM.render(<RecipePicker {...{day: dayName, section: sectionName}} />, picker);
+	})
 })
 
